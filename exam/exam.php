44741e95e19e19f39bf0416b22dc055d6974a9bc
aniket <chavan.andyaniket@gmail.com>
2020-04-13 22:00:40 +0530

    initial commit

---
+++ exam/exam.php
@@ -0,0 +1,16 @@
+<?php include 'inc/header.php'; ?>
+<?php
+Session::checkSession();
+?>
+<div class="main">
+<h1>Welcome to Online Exam - Start Now</h1>
+	</div>
+	<div class="segment">
+	<h2>Start Test</h2>
+	<ul>
+		<li><a href="starttest.php">Start Now...</a></li>
+	</ul>
+	</div>
+
+  </div>
+<?php include 'inc/footer.php'; ?>
